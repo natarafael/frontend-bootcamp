2fec2bb962169b9e63f0bfe5f57dc770a46a052c
Natã <nata.rcj@pti.org.br>
2022-06-22 21:08:50 -0300

    Remocao de coisas inuteis no App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import CadastroPeixes from "./pages/CadastroPeixes";
-import { Button } from "@mui/material";
 
 function App() {
-  const [teste, setTeste] = useState(true);
-
   return (
-    <div>
-      <h1>PIRACEMA</h1>
-      {teste && <CadastroPeixes />}
-    </div>
+    <>
+      <CadastroPeixes />
+    </>
   );
 }
 
