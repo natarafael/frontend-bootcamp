fd02efc32b7ea01535c9845342a00677ba4ee0df
Natã <nata.rcj@pti.org.br>
2022-06-25 17:25:05 -0300

    adicionado validação de numero positivo

--- src/pages/fish/FishRegistrationSchema.jsx
+++ src/pages/fish/FishRegistrationSchema.jsx
@@ -3,9 +3,7 @@ import * as Yup from 'yup';
 
 const FishRegistrationSchema =
     Yup.object({
-    pittag: Yup
-        .string()
-        .required('A pittag é obrigatória'),
+    pittag: Yup.string().required('A pittag é obrigatória'),
     scientificName: Yup
         .string()
         .required('O nome científico é obrigatório'),
@@ -14,12 +12,14 @@ const FishRegistrationSchema =
         .required('O nome comum é obrigatório'),
     totalLength: Yup
         .number()
+        .test('is-positive', 'O comprimento total deve ser um número positivo', value => value > 0)
         .required('O comprimento total é obrigatório'),
     captureLocation: Yup
         .string()
         .required('A localização de captura é obrigatória'),
     releaseWeight: Yup
         .number()
+        .test('is-positive', 'O peso de soltura deve ser um número positivo', value => value > 0)
         .required('O peso de soltura é obrigatório'),
     releaseDate: Yup
         .date()
@@ -29,6 +29,7 @@ const FishRegistrationSchema =
         .required('O local de soltura é obrigatório'),
     standardLength: Yup
         .number()
+        .test('is-positive', 'O comprimento padrão deve ser um número positivo', value => value > 0)
         .required('O comprimento padrão é obrigatório'),
     dnaSample: Yup
         .string()
