696bcb85a3b3ad51dc66f9707c116870f5d0552a
Douglas Machado <dgsekofoz@gmail.com>
2022-06-26 20:35:51 -0300

    switch no status antena

--- src/pages/status/EditStatus.jsx
+++ src/pages/status/EditStatus.jsx
@@ -1,10 +1,11 @@
 import useHookForm from "../../compents/hooks/UseHookForm";
 import {NewStatus} from "../../api/api";
-import {Box, Button} from "@mui/material";
+import {Box, Button,Typography,Switch} from "@mui/material";
 import Form from "../../compents/hook-forms/Form";
 import {ControlledTextField} from "../../compents/hook-forms/TextFieldForm";
 import StatusRegistrationSchema from "./StatusRegistrationSchema";
 import {toast} from "react-toastify";
+import {useState} from "react";
 
 const EditStatus = () => {
 
@@ -14,6 +15,8 @@ const EditStatus = () => {
         status:"",
         statusChangeDate:"",
     }
+
+    const [checked, setChecked] = useState(false);
 
     const FORM_ID = "NewStatus";
 
@@ -53,19 +56,24 @@ const EditStatus = () => {
                         label="Id da antena"
                         sx={{width: "48%", margin:"5px"}}
                     />
-                    <ControlledTextField
-                        name="status"
-                        control={methods.control}
-                        label="Status da antena"
-                        sx={{width: "48%", margin:"5px"}}
-                    />
+
                       <ControlledTextField
                         name="statusChangeDate"
                         control={methods.control}
                         label="Data de alteração do status"
                         sx={{width: "48%", margin:"5px"}}
                     />
-                    
+                    <Typography variant="inherit" p={2}>
+                        Status da Antena
+                        <Switch
+                            checked={checked}
+                            onChange={() => {
+                                setChecked(!checked)
+                                methods.setValue('status', !checked)
+                            }}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                        />
+                    </Typography>
                 <Box textAlign="center">
                     <Button
                         variant="contained"
